fb5cd1a782e5e775372e9c2e924c7433559787e4
Anderson Martins <BlackCode77.1980@gmail.com>
2023-07-11 08:28:58 -0300

    algoritmos recursivos

--- algoritmos/src/app/app.module.ts
+++ algoritmos/src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AlgoritmosModule } from "./algoritmos/algoritmos.module";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
     ],
     providers: [],
     bootstrap: [AppComponent],
---
+++ algoritmos/src/app/recursao-algoritmos-recursivos/recursao-algoritmos-recursivos/recursao-algoritmos-recursivos.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RecursaoAlgoritmosRecursivosComponent } from './recursao-algoritmos-recursivos.component';
+
+describe('RecursaoAlgoritmosRecursivosComponent', () => {
+  let component: RecursaoAlgoritmosRecursivosComponent;
+  let fixture: ComponentFixture<RecursaoAlgoritmosRecursivosComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ RecursaoAlgoritmosRecursivosComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(RecursaoAlgoritmosRecursivosComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
