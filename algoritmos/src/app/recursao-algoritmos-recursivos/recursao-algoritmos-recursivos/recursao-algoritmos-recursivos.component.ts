37081c2d653cba981e01f5646fbd6b591fd28caf
Anderson Martins <BlackCode77.1980@gmail.com>
2023-07-11 15:47:09 -0300

    Estudo de recursividade.

--- algoritmos/src/app/recursao-algoritmos-recursivos/recursao-algoritmos-recursivos/recursao-algoritmos-recursivos.component.ts
+++ algoritmos/src/app/recursao-algoritmos-recursivos/recursao-algoritmos-recursivos/recursao-algoritmos-recursivos.component.ts
@@ -8,7 +8,26 @@ import { Component } from '@angular/core';
 export class RecursaoAlgoritmosRecursivosComponent {
   /* https://www.ime.usp.br/~pf/algoritmos/aulas/recu.html */
 
+  /*Definindo um vetor*/
+  vetor = [1,2,3,4,5,6,7,8,9,10];
+  vetor1 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+  /*Definindo o tamanho do vetor*/
+  n = 7;
+  n1 = 10;
+
   constructor(){}
   ngOnInit(): void {}
+
+  /*Definindo uma função que encontra o maior elemento dentro de um vetor*/
+  maiorElementoDentroDeUmVetor(vetor: number[], n: number): number{ 
+    /*Caso base*/
+    if(n == 1){
+      return vetor[0];
+    }
+    /*Chamada recursiva*/
+    else{
+      return Math.max(vetor[n-1], this.maiorElementoDentroDeUmVetor(vetor, n-1));
+    }
+  }
 
 }
