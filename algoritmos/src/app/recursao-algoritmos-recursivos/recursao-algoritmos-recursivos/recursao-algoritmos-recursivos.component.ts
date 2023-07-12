447f903810abc47867e39226f722a542004a115d
Anderson Martins <BlackCode77.1980@gmail.com>
2023-07-11 21:46:00 -0300

    ...

--- algoritmos/src/app/recursao-algoritmos-recursivos/recursao-algoritmos-recursivos/recursao-algoritmos-recursivos.component.ts
+++ algoritmos/src/app/recursao-algoritmos-recursivos/recursao-algoritmos-recursivos/recursao-algoritmos-recursivos.component.ts
@@ -18,6 +18,39 @@ export class RecursaoAlgoritmosRecursivosComponent {
   constructor() {}
   ngOnInit(): void {}
 
+  /* Programa de teste.  Escreva um pequeno programa para testar a função recursiva maximo dada acima.  O seu 
+  programa deve gerar um vetor aleatório e encontrar um elemento máximo desse vetor. Acrescente ao seu programa 
+  uma função que confira a resposta dada por maximo.  [Solução: ./solucoes/recu2.html#part2] */
+  public vetor_1: number[] = [];
+  public n_1: number = 10;
+  programaDeTesteGeraVetorAleatorio(vetor_1: number[], n: number) {
+    // Gerando uma variavel de vetor aleatório
+	for (let i = 0; i < n; i++) {
+		vetor_1.push(Math.floor(Math.random() * 1000));
+	}
+	// encontrar um elemento máximo desse vetor
+	let maior = this.maiorElementoDentroDeUmVetor(vetor_1, n);
+	if(maior >= this.maiorElementoDentroDeUmVetor(vetor_1, n)){
+		return maior;
+	}else{
+		return this.maiorElementoDentroDeUmVetor(vetor_1, n);
+	}
+  }
+
+  /*8.Critique a seguinte função recursiva que promete encontrar o valor de um elemento máximo de v[0..n-1]*/
+  public fEVE = 8;
+  public fEVEVetor = [1013, 200, 330, 4, 5, 620, 730, 801];
+  funcEncValElemM(n: number, vetor: number[]): number {
+    if (n == 1) {
+      return vetor[0];
+    }
+    if (this.funcEncValElemM(n - 1, vetor) < vetor[n - 1]) {
+      return vetor[n - 1];
+    } else {
+      return this.funcEncValElemM(n - 1, vetor);
+    }
+  }
+
   /* 7. Critique a seguinte função recursiva que promete encontrar o valor de um elemento máximo de v[0..n-1]*/
   public fEVEMaximo = 8;
   public fEVEMaximoVetor = [101, 200, 33, 14, 45, 600, 732, 80];
@@ -40,7 +73,6 @@ export class RecursaoAlgoritmosRecursivosComponent {
     } else {
       return maior;
     }
-
   }
 
   /*Definindo uma função que encontra o maior elemento dentro de um vetor*/
